73c749452558cd66f3a1669c0fa7eb0036e797a7
cakescripter <cakescripter@gmail.com>
2023-07-11 22:13:37 +0200

    Commit

--- TypicodeTodos/script.js
+++ TypicodeTodos/script.js
@@ -4,24 +4,25 @@ const getTodos = () => {
   fetch(apiUrl + '?_limit=15')
     .then(res => res.json())
     .then(data => {
-      data.forEach((todo) => addTodoToDOM(todo));
+      data.forEach((todo) => addTodoToDOM(todo))
     });
 };
 
 const addTodoToDOM = (todo) => {
-  const div = document.createElement('div');
-  div.appendChild(document.createTextNode(todo.title));
+  const div = document.createElement('div')
+  div.classList.add('todo');
+  div.appendChild(document.createTextNode(todo.title))
   div.setAttribute('data-id', todo.id)
 
   if (todo.completed) {
-    div.classList.add('done');
+    div.classList.add('done')
   }
 
-  document.querySelector('#todo-list').appendChild(div);
+  document.querySelector('#todo-list').appendChild(div)
 }
 
 const createTodo = (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
 
   const newTodo = {
@@ -40,10 +41,42 @@ const createTodo = (e) => {
     .then(data => addTodoToDOM(data))
 }
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done')
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'))
+  }
+}
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+}
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(() => e.target.remove())
+  }
+}
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos)
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 }
 
 init();
-
