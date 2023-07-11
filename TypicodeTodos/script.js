1dacab8fc3479192e9471a3bfb3e683a2fc5effa
cakescripter <cakescripter@gmail.com>
2023-07-11 21:15:02 +0200

    Commit

--- TypicodeTodos/script.js
+++ TypicodeTodos/script.js
@@ -1,20 +1,49 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-  fetch(apiUrl + '?_limit=10')
+  fetch(apiUrl + '?_limit=15')
     .then(res => res.json())
     .then(data => {
-      data.forEach((todo) => {
-        const div = document.createElement('div');
-        div.appendChild(document.createTextNode(todo.title));
-
-        if (todo.completed) {
-          div.classList.add('done');
-        }
-
-        document.querySelector('#todo-list').appendChild(div);
-      })
+      data.forEach((todo) => addTodoToDOM(todo));
     });
 };
 
-getTodos();
+const addTodoToDOM = (todo) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id)
+
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+
+  document.querySelector('#todo-list').appendChild(div);
+}
+
+const createTodo = (e) => {
+  e.preventDefault();
+
+
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false
+  }
+
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(data => addTodoToDOM(data))
+}
+
+const init = () => {
+  document.addEventListener('DOMContentLoaded', getTodos)
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+}
+
+init();
+
