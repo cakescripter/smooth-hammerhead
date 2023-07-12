e2229b65977c9a74f81fa35f044c336ee270ca22
cakescripter <cakescripter@gmail.com>
2023-07-12 17:17:53 +0200

    Initial Commit

---
+++ TVMaze/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const img = document.createElement('img');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img)
+})
