63abebe8286984d5a41538bfc1b6c2eb0e2a054b
cakescripter <cakescripter@gmail.com>
2023-07-12 17:54:01 +0200

    Commit

--- TVMaze/script.js
+++ TVMaze/script.js
@@ -2,8 +2,27 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    const img = document.createElement('img');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img)
-})
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+
+    removeImages();
+    makeImages(res.data);
+    form.elements.query.value = '';
+});
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
+
+const removeImages = () => {
+    const images = document.querySelectorAll('img');
+    images.forEach((image) => {
+        image.remove();
+    });
+}
